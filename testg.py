8a3614f1612fd559ff1355470c7f9a6cfb88ecd2
James <jamesisom310@gmail.com>
2022-02-23 17:07:11 -0500

    Added additional test case

--- testg.py
+++ testg.py
@@ -5,10 +5,17 @@ from gil_r import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
+        items = [Item(name="foo", sell_in=10, quality=20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("foo", items[0].name)
+        
+    def test_foo2(self):
         items = [Item(name="Aged Brie", sell_in=10, quality=20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("+5 Dexterity Vest", items[0].name)
+        self.assertEqual(21, items[0].quality)
+        self.assertEqual(9, items[0].sell_in)
         # self.assertEqual(10, items[0].sell_in)
 
         
